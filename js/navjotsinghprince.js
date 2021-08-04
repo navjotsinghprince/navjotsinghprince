d2d75ca7439b0473f4898f04b08baa6345638a2f
Navjot Singh <59249017+navjotsinghprince@users.noreply.github.com>
2021-08-04 20:35:23 +0530

    Navjotsinghprince Javascript Added

---
+++ js/navjotsinghprince.js
@@ -0,0 +1,39 @@
+//My Details
+function getAge(dateString) {
+  var today = new Date();
+  var birthDate = new Date(dateString);
+  var age = today.getFullYear() - birthDate.getFullYear();
+  var m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+  return age + "Years";
+}
+
+let my = {
+  Name: "Navjot singh",
+  Age: getAge("1997/05/26"),
+  Residence: "Punjab, India",
+  Address: "Kasu Begu Ferozepur Cantt 152004",
+  Email: "navjotsinghprince1@gmail.com",
+  Phone: "(+91)8591373660",
+  Freelance: "Unavailable",
+  Download_Resume:
+    "https://www.mediafire.com/file/bw8i4vdrgzg7c6k/Navjot__Singh_Resume.pdf/file",
+};
+
+var nodes = document.getElementById("about-info").getElementsByTagName("li");
+for (var i = 0; i < nodes.length; i++) {
+  if (nodes[i].childNodes[0].innerHTML == "Name") {
+    // console.log((nodes[i].childNodes[1].innerHTML = "Hello World"));
+  }
+}
+
+function initialization(check) {
+  switch (check) {
+    case "Name":
+      break;
+    case "Age":
+      break;
+  }
+}
